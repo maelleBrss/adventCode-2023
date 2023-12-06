a205aa55784c7e85e366c360070d65b66c2bed80
maelleBrss <maelle.brss@gmail.com>
2023-12-06 12:36:19 +0100

    add day 2 part 2

--- days/day2/cube.py
+++ days/day2/cube.py
@@ -1,6 +1,7 @@
 import sys
 from collections import defaultdict
 from typing import List
+from functools import reduce
 
 
 def nb_cubes_higher(color: str, dict_cubes: dict) -> bool:
@@ -38,11 +39,45 @@ def compute_games(input_lines: List[str]) -> List[tuple]:
 
     return list_games
 
-def cube_conundrum(input_lines: List[str]) -> None:
+
+def min_cubes(input_lines: List[str]) -> None:
+    # don't mind the duplicate code for now
+    list_powers = []
+
+    for line in input_lines:
+        line_without_id = line.split(':')[1][1:]
+        list_round = list(map(str.strip, line_without_id.split(';')))
+
+        list_dict = []
+        for round in list_round:
+            dict_cube_round = defaultdict(int)
+            split_cubes = list(map(str.strip, round.split(',')))
+            for cube in split_cubes:
+                split_info = cube.split()
+                dict_cube_round[split_info[1]] = int(split_info[0])
+            list_dict.append(dict_cube_round)
+
+        list_max_value = [max(list_dict, key=lambda x: x[col])[col]
+                          for col in original_bag.keys()]
+
+        power_values = reduce(lambda x, y: x * y, list_max_value)
+        list_powers.append(power_values)
+
+    sum_total = sum(int(power) for power in list_powers)
+    print(sum_total)
+
+def guess_nb_cubes(input_lines: List[str]) -> None:
     list_all_games = compute_games(input_lines)
     list_possible_games = list(filter(lambda g: g[1], list_all_games))
     sum_id = sum(int(game[0]) for game in list_possible_games)
     print(sum_id)
+
+
+
+def cube_conundrum(input_lines: List[str]) -> None:
+    guess_nb_cubes(input_lines)
+    min_cubes(input_lines)
+
 
 if __name__ == '__main__':
     global original_bag
